552946bdc843e2fc7850327ee89c29421e99baa0
PracticeKJY <poiijtl1004@naver.com>
2023-05-26 22:57:43 +0900

    ✨ feat. Heading 컴포넌트 생성
    - title, subTitle을 렌더링하는 Heading 컴포넌트 생성

---
+++ pages/@component/Heading.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { FC } from "react"
+
+interface HeadingProps {
+  title: string
+  subTitle: string
+  center?: boolean
+}
+
+const Heading: FC<HeadingProps> = ({ title, subTitle, center }) => {
+  return (
+    <div style={{ textAlign: center ? "center" : "start" }}>
+      <div style={{ fontSize: "1.5rem", fontWeight: "700" }}>{title}</div>
+      <div
+        style={{
+          fontWeight: "300",
+          marginTop: "0.5rem",
+          color: "#737373",
+        }}
+      >
+        {subTitle}
+      </div>
+    </div>
+  )
+}
+
+export default Heading
