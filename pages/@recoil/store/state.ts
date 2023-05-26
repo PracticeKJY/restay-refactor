0971ae82a0d5fb67a50c6a0d36cd4bbbf80763f4
PracticeKJY <poiijtl1004@naver.com>
2023-05-26 22:48:47 +0900

    ✨ feat. 로그인, 회원가입 모달의 열린 유무를 가진 상태 선언
    - recoil을 이용하여 useLoginModal, useRegisterModal 생성

--- pages/@recoil/store/state.ts
+++ pages/@recoil/store/state.ts
@@ -6,3 +6,12 @@ export const nameAtom = atom({
   key: "nameAtom",
   default: "조함찌",
 })
+
+export const useLoginModal = atom({
+  key: "useLoginModal",
+  default: false,
+})
+export const useRegisterModal = atom({
+  key: "useRegisterModal",
+  default: false,
+})
