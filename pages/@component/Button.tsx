120d87612aeff71a06d13f061bc47fdd52b4ff32
PracticeKJY <poiijtl1004@naver.com>
2023-05-26 22:52:27 +0900

    ✨ feat. Button 컴포넌트 생성
    - outline , small 의 유무에 따라 스타일링이 변하는 button 컴포넌트 생성

---
+++ pages/@component/Button.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { FC } from "react"
+import { IconType } from "react-icons"
+import Image from "next/image"
+
+import styles from "./Button.module.css"
+
+interface ButtonProps {
+  label: string
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+  disabled?: boolean
+  outline?: boolean
+  small?: boolean
+  icon?: IconType | string
+  alt?: string
+}
+
+const Button: FC<ButtonProps> = ({
+  label,
+  onClick,
+  disabled,
+  outline,
+  small = false,
+  icon: Icon,
+  alt,
+}) => {
+  // const modalTransitionClass = showModal ? styles.modalShow : styles.modalHide
+
+  const outlineClass = outline ? styles.isOutline : styles.nonOutline
+  const smallClass = small ? styles.isSmall : styles.nonSmall
+
+  // <div className={`${styles.button} ${outlineClass} ${smallClass}`}>
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${outlineClass} ${smallClass}`}
+    >
+      {typeof Icon === "string" ? (
+        <Image
+          src={Icon}
+          alt={alt || ""}
+          width={25}
+          height={25}
+          className={styles.Image}
+        />
+      ) : (
+        Icon && <Icon size={24} className={styles.Image} />
+      )}
+      {label}
+    </button>
+  )
+}
+
+export default Button
