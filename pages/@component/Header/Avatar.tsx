8fa43989472c36043118d2a36d56396f985ff61d
PracticeKJY <poiijtl1004@naver.com>
2023-06-11 00:17:52 +0900

    🎨 modify. Avatar 컴포넌트 props 수정
    - width와 height 속성을 추가하였습니다.

--- pages/@component/Header/Avatar.tsx
+++ pages/@component/Header/Avatar.tsx
@@ -3,14 +3,20 @@ import avatar from "/public/placeHolder.jpg"
 import { FC } from "react"
 type AccountImageProps = {
   accountImage?: string | null | undefined
+  width?: number
+  height?: number
 }
 
-const Avatar: FC<AccountImageProps> = ({ accountImage }) => {
+const Avatar: FC<AccountImageProps> = ({
+  accountImage,
+  width: Width,
+  height: Height,
+}) => {
   return (
     <Image
       style={{ borderRadius: "9999px" }}
-      width={30}
-      height={30}
+      width={Width ? Width : 30}
+      height={Height ? Height : 30}
       alt="Avatar"
       src={accountImage || avatar}
     />
