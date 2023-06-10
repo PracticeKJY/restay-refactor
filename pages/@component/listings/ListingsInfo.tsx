21c13b09374557583c7ded3bdcfd15ff34a191be
PracticeKJY <poiijtl1004@naver.com>
2023-06-10 16:54:55 +0900

    ✨ feat. DetailListingBody부분 추가
    - 바디부분 중 하나인 카테고리, 상세정보, 위치 부분을 추가하였습니다.

---
+++ pages/@component/listings/ListingsInfo.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import styles from "./ListingsInfo.module.css"
+import useCountries from "@/pages/@hooks/useCountries"
+import dynamic from "next/dynamic"
+import { FC, useMemo } from "react"
+import { categories } from "../Header/Categories"
+import ListingCategory from "./ListingCategory"
+
+interface ListingsInfoProps {
+  listingData: {
+    category: string
+    locationValue: string
+    guestCount: number
+    roomCount: number
+    bathroomCount: number
+    imageSrc: string[] | undefined
+    price: number
+    title: string
+    description: string
+    userId: any
+    createdAt: any
+    _id: string
+  }
+}
+
+const ListingsInfo: FC<ListingsInfoProps> = ({ listingData }) => {
+  const { getByValue } = useCountries()
+  const location = getByValue(listingData.locationValue)
+
+  const MapComponent = useMemo(
+    () =>
+      dynamic(() => import("@/pages/@component/MapComponent"), {
+        ssr: false,
+      }),
+    [location],
+  )
+
+  const findCategory = categories.filter((item) => {
+    return item.label === listingData.category
+  })
+
+  return (
+    <div className={styles.hostDataContainer}>
+      <div className={styles.hostingIntroduceInfo}>
+        <div>
+          <h2 className={styles.hostingIntroduce}>함찌 님이 호스팅하는 숙소</h2>
+        </div>
+        <ol className={styles.countList}>
+          <li>{`최대 인원 ${listingData.guestCount}명`}</li>
+          <li>{`방 ${listingData.roomCount}개`}</li>
+          <li>{`욕실 ${listingData.bathroomCount}개`}</li>
+        </ol>
+      </div>
+      <div className={styles.locationInfo}>
+        {findCategory && (
+          <ListingCategory
+            icon={findCategory[0].icon}
+            label={findCategory[0]?.label}
+            description={findCategory[0]?.description}
+          />
+        )}
+      </div>
+      <div className={styles.descriptionInfo}>
+        <div>{listingData.description}</div>
+      </div>
+      <div className={styles.mapInfo}>
+        <div>호스팅 지역</div>
+        <MapComponent center={location?.latlng} />
+      </div>
+    </div>
+  )
+}
+
+export default ListingsInfo
