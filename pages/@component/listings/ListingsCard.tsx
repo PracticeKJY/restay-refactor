4edeac337a696c83d5438bf3ba2e1ff1b171e9ca
PracticeKJY <poiijtl1004@naver.com>
2023-06-10 16:55:42 +0900

    ✨ feat. 홈 화면 상품카드에 swiper 추가

--- pages/@component/listings/ListingsCard.tsx
+++ pages/@component/listings/ListingsCard.tsx
@@ -1,48 +1,118 @@
 "use client"
 
 import { FC, useState } from "react"
+import { useRouter } from "next/navigation"
 import styles from "./ListingsCard.module.css"
 import Image from "next/image"
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
-import { useRecoilState } from "recoil"
-import { hasFavoriteAtom } from "@/pages/@recoil/store/state"
 import useCountries from "@/pages/@hooks/useCountries"
+import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore, { Swiper as SwiperType, Navigation } from "swiper"
+import { useRef } from "react"
 
-const ListingsCard = ({ data, userId }) => {
-  // const [hasFavorite, setHasFavorite] = useRecoilState(hasFavoriteAtom)
+import "swiper/css"
+import "swiper/css/navigation"
+
+SwiperCore.use([Navigation])
+
+interface ListingsCardProps {
+  data: any
+  userId: any
+}
+
+const ListingsCard: FC<ListingsCardProps> = ({ data, userId }) => {
   const [hasFavorite, setHasFavorite] = useState(false)
+  const [isHover, setIsHover] = useState(false)
+
+  const router = useRouter()
+
   const handleSetFavorite = () => {
     setHasFavorite(!hasFavorite)
   }
   const { getByValue } = useCountries()
   const location = getByValue(data.locationValue)
 
+  const productCards = data.imageSrc.map((src: string, index: any) => {
+    return (
+      <SwiperSlide key={index}>
+        <Image
+          width={350}
+          height={350}
+          alt=""
+          src={src}
+          className={styles.listingsImage}
+          onClick={() => {
+            router.push(`/listings/${data._id}`)
+          }}
+          priority
+        />
+      </SwiperSlide>
+    )
+  })
+
+  const swiperRef = useRef<SwiperType>()
+
   return (
-    <div className={styles.listingsWrapper}>
-      <Image
-        width={350}
-        height={350}
-        alt=""
-        src={data.imageSrc ? data.imageSrc[0] : ""}
-        className={styles.listingsImage}
-      />
-      <div onClick={handleSetFavorite}>
-        {hasFavorite ? (
-          <AiFillHeart size={28} className={styles.aiFillHeart} />
-        ) : (
-          <AiOutlineHeart size={28} className={styles.aiOutlineHeart} />
-        )}
+    <>
+      <div
+        className={styles.listingsWrapper}
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <Swiper
+          className={styles.swiper}
+          modules={[Navigation]}
+          loop
+          slidesPerGroup={1}
+          slidesPerView={1}
+          spaceBetween={0}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper
+          }}
+        >
+          {productCards}
+        </Swiper>
+        <div onClick={handleSetFavorite}>
+          {hasFavorite ? (
+            <AiFillHeart size={28} className={styles.aiFillHeart} />
+          ) : (
+            <AiOutlineHeart size={28} className={styles.aiOutlineHeart} />
+          )}
+        </div>
+        <button
+          className={`${styles.prevButton} ${
+            isHover ? styles.visibleButton : ""
+          }`}
+          onClick={() => swiperRef.current?.slidePrev()}
+        >
+          {"<"}
+        </button>
+        <button
+          className={`${styles.nextButton} ${
+            isHover ? styles.visibleButton : ""
+          }`}
+          onClick={() => swiperRef.current?.slideNext()}
+        >
+          {">"}
+        </button>
+
+        <div
+          onClick={() => {
+            router.push(`/listings/${data._id}`)
+          }}
+        >
+          <div className={styles.listingsLocation}>
+            {location?.region}, {location?.label}
+          </div>
+          <div className={styles.listingsCategory}>{data.category}</div>
+          <div className={styles.listingsTitle}>{data.title}</div>
+          <div>
+            <span className={styles.listingsPrice}>￦{data.price}</span>
+            /박
+          </div>
+        </div>
       </div>
-      <div className={styles.listingsLocation}>
-        {location?.region}, {location?.label}
-      </div>
-      <div className={styles.listingsCategory}>{data.category}</div>
-      <div className={styles.listingsTitle}>{data.title}</div>
-      <div>
-        <span className={styles.listingsPrice}>￦{data.price}</span>
-        /박
-      </div>
-    </div>
+    </>
   )
 }
 
