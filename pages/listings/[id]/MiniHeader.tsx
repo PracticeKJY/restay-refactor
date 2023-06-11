e10199c6ec3423feaa303ff9b0a2d3fa53501e64
PracticeKJY <poiijtl1004@naver.com>
2023-06-11 19:31:59 +0900

    ✨ feat. 미니헤더 제작
    - 상품 상세 페이지에서 스크롤을 일정만큼 내릴 시 상위에 고정되는 미니헤더를 제작하였습니다.
    
    - 사진, 숙소정보, 위치 목록 별 클릭시 텍스트에 맞는 항목으로 이동하도록 스크롤이벤트를 구현하였습니다.

---
+++ pages/listings/[id]/MiniHeader.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import styles from "./DetailListingPage.module.css"
+import { useEffect, useState } from "react"
+
+const MiniHeader = ({ topRef, bodyRef, mapRef }: any) => {
+  const [isShow, setIsShow] = useState(false)
+
+  const moveToTop = () => {
+    if (topRef) {
+      topRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+  const moveToBodyInfo = () => {
+    if (bodyRef) {
+      bodyRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+  const moveToMapInfo = () => {
+    if (mapRef) {
+      mapRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 80) {
+        setIsShow(true)
+      } else {
+        setIsShow(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  return (
+    <div className={`${isShow ? styles.displayNav : styles.notdisplayNav}`}>
+      <div className={styles.detailListingNavContainer}>
+        <div className={styles.detailListingNavWrapper}>
+          <div onClick={moveToTop}>사진</div>
+          <div onClick={moveToBodyInfo}>숙소 정보</div>
+          <div onClick={moveToMapInfo}>위치</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MiniHeader
