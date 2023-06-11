b919284813e09df119e32552df57af672500176d
PracticeKJY <poiijtl1004@naver.com>
2023-06-11 19:37:11 +0900

    🎨 modify. 제품 상세 페이지 컴포넌트 분리
    - 유지보수 및 가독성을 위해 DetailListingMainCarouselSwiper.tsx을 생성하여 컴포넌트로 분리하였습니다

---
+++ pages/listings/[id]/DetailListingMainCarouselSwiper.tsx
@@ -0,0 +1,132 @@
+"use client"
+
+import styles from "./DetailListingPage.module.css"
+
+import { FC, useRef, useState } from "react"
+
+import Image from "next/image"
+import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore, {
+  Swiper as SwiperType,
+  Navigation,
+  FreeMode,
+  Thumbs,
+} from "swiper"
+
+import "swiper/css"
+import "swiper/css/navigation"
+
+interface DetailListingMainCarouselSwiperProps {
+  listingData: {
+    category: string
+    locationValue: string
+    guestCount: number
+    roomCount: number
+    bathroomCount: number
+    imageSrc: string[] | undefined
+    price: number
+    title: string
+    description: string
+    userId: any
+    createdAt: any
+    _id: string
+  }
+}
+
+SwiperCore.use([Navigation])
+
+const DetailListingMainCarouselSwiper: FC<
+  DetailListingMainCarouselSwiperProps
+> = ({ listingData }) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null)
+  const swiperRef = useRef<SwiperType>()
+  const [slideIndex, setslideIndex] = useState(0)
+
+  const findSwiperIndexHandler = (swiper: any) => {
+    setslideIndex(swiper.realIndex)
+  }
+
+  const productCards = listingData.imageSrc?.map((src: string, index: any) => {
+    return (
+      <SwiperSlide key={index}>
+        <Image
+          width={560}
+          height={348}
+          alt=""
+          src={src}
+          className={styles.productCards}
+          priority
+        />
+      </SwiperSlide>
+    )
+  })
+  const productCardsList = listingData.imageSrc?.map(
+    (src: string, index: any) => {
+      return (
+        <SwiperSlide
+          key={index}
+          className={`${styles.swiperSlide} ${
+            slideIndex === index ? styles.active : ""
+          }`}
+        >
+          <Image
+            width={120}
+            height={100}
+            alt=""
+            src={src}
+            className={styles.productCardsListImage}
+            priority
+          />
+        </SwiperSlide>
+      )
+    },
+  )
+
+  return (
+    <div className={styles.swiperWrapper}>
+      <Swiper
+        className={styles.swiper}
+        onSlideChange={findSwiperIndexHandler}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+        }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        loop
+        slidesPerGroup={1}
+        slidesPerView={1}
+        spaceBetween={0}
+        onBeforeInit={(swiper) => {
+          swiperRef.current = swiper
+        }}
+      >
+        {productCards}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={0}
+        slidesPerView={4}
+        freeMode={true}
+        loop
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className={styles.swiperList}
+      >
+        {productCardsList}
+      </Swiper>
+      <button
+        className={styles.prevButton}
+        onClick={() => swiperRef.current?.slidePrev()}
+      >
+        {"<"}
+      </button>
+      <button
+        className={styles.nextButton}
+        onClick={() => swiperRef.current?.slideNext()}
+      >
+        {">"}
+      </button>
+    </div>
+  )
+}
+
+export default DetailListingMainCarouselSwiper
--- pages/listings/[id]/DetailListingPage.tsx
+++ pages/listings/[id]/DetailListingPage.tsx
@@ -2,25 +2,16 @@
 
 import styles from "./DetailListingPage.module.css"
 
-import Container from "@/pages/@component/Container"
-import { FC, useMemo, useRef, useState } from "react"
-
-import Image from "next/image"
-import { Swiper, SwiperSlide } from "swiper/react"
-import SwiperCore, {
-  Swiper as SwiperType,
-  Navigation,
-  FreeMode,
-  Thumbs,
-} from "swiper"
-
-import "swiper/css"
-import "swiper/css/navigation"
-import ListingsInfo from "@/pages/@component/listings/ListingsInfo"
-import DetailListingHeading from "./DetailListingHeading"
-import ListingsReservation from "@/pages/@component/listings/ListingsReservation"
+import { FC, useMemo, useRef } from "react"
 import useCountries from "@/pages/@hooks/useCountries"
 import dynamic from "next/dynamic"
+
+import Container from "@/pages/@component/Container"
+import ListingsInfo from "@/pages/@component/listings/ListingsInfo"
+import ListingsReservation from "@/pages/@component/listings/ListingsReservation"
+import DetailListingHeading from "./DetailListingHeading"
+import DetailListingMainCarouselSwiper from "./DetailListingMainCarouselSwiper"
+import MiniHeader from "./MiniHeader"
 
 interface DetailListingMainCarouselSwiperProps {
   listingData: {
@@ -43,12 +34,13 @@ interface DetailListingPageProps extends DetailListingMainCarouselSwiperProps {
   userInfo: any
 }
 
-SwiperCore.use([Navigation])
-
 const DetailListingPage: FC<DetailListingPageProps> = ({
   listingData,
   userInfo,
 }) => {
+  const topRef = useRef(null)
+  const bodyRef = useRef(null)
+  const mapRef = useRef(null)
   const { getByValue } = useCountries()
   const location = getByValue(listingData.locationValue)
   const MapComponent = useMemo(
@@ -60,119 +52,33 @@ const DetailListingPage: FC<DetailListingPageProps> = ({
   )
 
   return (
-    <Container>
-      <div className={styles.contentContainer}>
-        <div className={styles.mainCarousel}>
-          <DetailListingMainCarouselSwiper listingData={listingData} />
-          <DetailListingHeading listingData={listingData} userInfo={userInfo} />
-        </div>
-        <div className={styles.bodyContainer}>
-          <ListingsInfo listingData={listingData} userInfo={userInfo} />
-          <ListingsReservation price={listingData.price} />
-        </div>
-        <div className={styles.mapInfoWrapper}>
-          <div className={styles.mapInfo}>
-            <div className={styles.mapInfoTitle}>호스팅 위치</div>
-            <MapComponent center={location?.latlng} />
+    <>
+      <MiniHeader topRef={topRef} bodyRef={bodyRef} mapRef={mapRef} />
+      <div ref={topRef}>
+        <Container>
+          <div className={styles.contentContainer}>
+            <div className={styles.mainCarousel}>
+              <DetailListingMainCarouselSwiper listingData={listingData} />
+              <DetailListingHeading
+                listingData={listingData}
+                userInfo={userInfo}
+              />
+            </div>
+            <div className={styles.bodyContainer} ref={bodyRef}>
+              <ListingsInfo listingData={listingData} userInfo={userInfo} />
+              <ListingsReservation price={listingData.price} />
+            </div>
+            <div className={styles.mapInfoWrapper}>
+              <div className={styles.mapInfo} ref={mapRef}>
+                <div className={styles.mapInfoTitle}>호스팅 위치</div>
+                <MapComponent center={location?.latlng} />
+              </div>
+            </div>
           </div>
-        </div>
+        </Container>
       </div>
-    </Container>
+    </>
   )
 }
 
 export default DetailListingPage
-
-const DetailListingMainCarouselSwiper: FC<
-  DetailListingMainCarouselSwiperProps
-> = ({ listingData }) => {
-  const [thumbsSwiper, setThumbsSwiper] = useState<any>(null)
-  const swiperRef = useRef<SwiperType>()
-  const [slideIndex, setslideIndex] = useState(0)
-
-  const findSwiperIndexHandler = (swiper: any) => {
-    setslideIndex(swiper.realIndex)
-  }
-
-  const productCards = listingData.imageSrc?.map((src: string, index: any) => {
-    return (
-      <SwiperSlide key={index}>
-        <Image
-          width={560}
-          height={348}
-          alt=""
-          src={src}
-          className={styles.productCards}
-          priority
-        />
-      </SwiperSlide>
-    )
-  })
-  const productCardsList = listingData.imageSrc?.map(
-    (src: string, index: any) => {
-      return (
-        <SwiperSlide
-          key={index}
-          className={`${styles.swiperSlide} ${
-            slideIndex === index ? styles.active : ""
-          }`}
-        >
-          <Image
-            width={120}
-            height={100}
-            alt=""
-            src={src}
-            className={styles.productCardsListImage}
-            priority
-          />
-        </SwiperSlide>
-      )
-    },
-  )
-
-  return (
-    <div className={styles.swiperWrapper}>
-      <Swiper
-        className={styles.swiper}
-        onSlideChange={findSwiperIndexHandler}
-        thumbs={{
-          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-        }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        loop
-        slidesPerGroup={1}
-        slidesPerView={1}
-        spaceBetween={0}
-        onBeforeInit={(swiper) => {
-          swiperRef.current = swiper
-        }}
-      >
-        {productCards}
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={0}
-        slidesPerView={4}
-        freeMode={true}
-        loop
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className={styles.swiperList}
-      >
-        {productCardsList}
-      </Swiper>
-      <button
-        className={styles.prevButton}
-        onClick={() => swiperRef.current?.slidePrev()}
-      >
-        {"<"}
-      </button>
-      <button
-        className={styles.nextButton}
-        onClick={() => swiperRef.current?.slideNext()}
-      >
-        {">"}
-      </button>
-    </div>
-  )
-}
