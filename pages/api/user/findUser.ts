cf2eaed391815ddc5dfad8e166c22183129e54d1
PracticeKJY <poiijtl1004@naver.com>
2023-06-15 15:07:38 +0900

    🎨 modify. 테스트용 콘솔 제거

--- pages/api/user/findUser.ts
+++ pages/api/user/findUser.ts
@@ -10,7 +10,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       .collection("users")
       .findOne({ _id: new ObjectId(req.body.id) })
 
-    console.log(result, "바디스")
     return res.status(200).json(result)
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" })
