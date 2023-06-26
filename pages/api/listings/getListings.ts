049595a82e3fa03cec355c1f2f9daa2ed6bae343
PracticeKJY <poiijtl1004@naver.com>
2023-06-26 15:29:35 +0900

    🎨 modify. getListings.ts 코드 수정
    - validation을 추가하였습니다.

--- pages/api/listings/getListings.ts
+++ pages/api/listings/getListings.ts
@@ -3,13 +3,15 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import connectDB from "@/pages/@lib/mongoDB"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const db = (await connectDB).db("Restay")
-    const result = await db.collection("listings").find().toArray()
+  if (req.body) {
+    try {
+      const db = (await connectDB).db("Restay")
+      const result = await db.collection("listings").find(req.body).toArray()
 
-    return res.status(200).json(result)
-  } catch (error) {
-    return res.status(500).json({ error: "Internal Server Error" })
+      return res.status(200).json(result)
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error" })
+    }
   }
 }
 
