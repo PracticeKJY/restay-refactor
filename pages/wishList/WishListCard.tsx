4a90de26e726304a004b1eeacab9085ad7eaac34
PracticeKJY <poiijtl1004@naver.com>
2023-06-29 19:11:21 +0900

    🎨 modify. 위시리스트 항목 레이아웃 변경
    - WishListCard.tsx의 div element를 추가하고 css 속성을 수정하였습니다.
    - WishListCard.tsx 에서 컴포넌트 분리 및 수정을 통해 위시리스트 항목이 없을 시 특정 텍스트와 버튼을 띄우도록 수정하였습니다.

--- pages/wishList/WishListCard.tsx
+++ pages/wishList/WishListCard.tsx
@@ -3,12 +3,11 @@
 import styles from "./WishListCard.module.css"
 
 import { useAtomValue } from "jotai"
-import { Suspense } from "react"
 import { wishListAtom } from "../../jotai/@store/state"
 import Container from "../@component/Container"
 import { useSession } from "next-auth/react"
 import Card from "./Card"
-import Loading from "../Loading"
+import Empty from "../@component/Empty"
 
 const Content = () => {
   const { data: session } = useSession()
@@ -23,22 +22,39 @@ const Content = () => {
   return (
     <>
       <div className={styles.wishListContainer}>
-        <div className={styles.wishListTitle}>위시리스트</div>
-        <div className={styles.listingsContainer}>
-          {wishListData.map((data: any) => {
-            return (
-              <Card
-                key={data._id}
-                userId={data._id}
-                title={data.wishListTitle}
-                data={data.favoriteData[0]}
-                disable={true}
-              />
-            )
-          })}
-        </div>
+        {wishListData.length > 0 ? (
+          <WishListProduct wishListData={wishListData} />
+        ) : (
+          <Empty
+            title="첫 번째 위시리스트를 만들어 주세요!"
+            subtitle="하트 아이콘을 누르시고 가고싶은 숙소를 위시리스트에 저장하세요."
+            actionLabel="숙소 검색하기"
+            showReset
+          />
+        )}
       </div>
     </>
+  )
+}
+
+const WishListProduct = ({ wishListData }: any) => {
+  return (
+    <div className={styles.wishListWrapper}>
+      <div className={styles.wishListTitle}>위시리스트</div>
+      <div className={styles.listingsContainer}>
+        {wishListData.map((data: any) => {
+          return (
+            <Card
+              key={data._id}
+              userId={data._id}
+              title={data.wishListTitle}
+              data={data.favoriteData[0]}
+              disable={true}
+            />
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
